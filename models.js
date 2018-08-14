93f463e84295469f57b5204287ef0380807993cd
Juan Diego Donneys <juandiego@Juans-MacBook-Pro.local>
2018-08-14 10:22:54 -0700

    mongoose user and task models

---
+++ models.js
@@ -0,0 +1,38 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+  googleCalendarAccount: {
+    accessToken: String,
+    refreshToken: String
+  },
+  slackID: String,
+  slackUsername: String,
+  slackEmail: String,
+  slackDmId: String,
+  meetingLength: {
+    type: Number,
+    default: 30
+  }
+});
+
+var taskSchema = new Schema({
+  subject: {
+    type: String,
+    required: true
+  },
+  day: {
+    type: String,
+    required: true
+  },
+  googleCalendarEventId: String,
+  requesterid: String
+});
+
+var User = mongoose.model('User', userSchema);
+var Task = mongoose.model('Task', taskSchema);
+
+module.exports = {
+  User: User,
+  Task: Task
+}
--- server.js
+++ server.js
@@ -1,9 +1,36 @@
-const { WebClient } = require('@slack/client');
+import { RTMClient, WebClient } from '@slack/client';
+import express from 'express'
+import bodyParser from 'body-parser'
+import {google} from 'googleapis'
+const fs = require('fs');
+const readline = require('readline');
+
+const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
+const TOKEN_PATH = 'token.json';
 
 // An access token (from your Slack app or custom integration - xoxp, xoxb, or xoxa)
 const token = process.env.SLACK_TOKEN;
 
 const web = new WebClient(token);
+
+const rtm = new RTMClient(token);
+
+rtm.start();
+
+rtm.on('message', function(event) {
+  console.log(event);
+  if (event.type !== 'message') {
+    return
+  }
+  handleMessage(event);
+})
+
+handleMessage = (event) => {
+  axios.get(`url goes here`, )
+  .then(res => {
+
+  })
+}
 
 // This argument can be a channel ID, a DM ID, a MPDM ID, or a group ID
 const conversationId = 'DC7RFR5B7';
